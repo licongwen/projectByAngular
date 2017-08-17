3d098c13991ccbdddcaa11e386743a4e3a0914b5
licongwen <649091795@qq.com>
2017-08-17 23:02:55 +0800

    ''

--- app/photo/wuhu/wuhuController.js
+++ app/photo/wuhu/wuhuController.js
@@ -6,31 +6,26 @@ angular.module('app')
 		
 		function AlertImg(){
 			var oDiv=document.createElement('div');
-			oDiv.setAttribute('id','divHide');
-			oDiv.setAttribute('class','divHide');
-			document.body.appendChild(oDiv);
-
+				oDiv.setAttribute('id','divHide');
+				oDiv.setAttribute('class','divHide');
+				document.body.appendChild(oDiv);
 
 			var oImg=document.createElement('img');
-			oImg.setAttribute('class','image');
-			oImg.src=this.getAttribute('src');
-			
+				oImg.setAttribute('class','addImage');
+				oImg.src=this.getAttribute('src');
+			//console.log(oImg.src);
+			$('#divHide').append(oImg);
 
-			
-			console.log(oImg.src);
-
-			oImg.setAttribute('alt','ldsdsd');
-			$('#divHide').append('oImg');
-			oImg.onclick=restore;
+			oDiv.onclick=restore;
 		}
 		function restore(){
-		     document.body.removeChild(document.getElementById("overlay"));
-		     document.body.removeChild(document.getElementById("img"));
+		     document.body.removeChild(document.getElementById("divHide"));
 		 }
 
 		var Img=$('.listImg');
 
 		for(var i=0;i<Img.length;i++){
 			Img[i].onclick=AlertImg;
+
 		}
 	}])
